a3c13baad4f88cec92d97f76feee9904ea85a674
Ruslan Bay <67730802+ruslanbay@users.noreply.github.com>
2025-02-08 14:44:59 +0000

    version bump

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 plotly-3.0.0.min.js"use strict";
 
-const CACHE_NAME = "my-app-static-cache-v3";
-const DATA_CACHE_NAME = "my-app-data-cache-v3";
+const CACHE_NAME = "my-app-static-cache-v4";
+const DATA_CACHE_NAME = "my-app-data-cache-v4";
 
 const FILES_TO_CACHE = [
   "/",
