41afe662f93cd9e66ea66a63da99438acb7c55dd
ruslanbay <67730802+ruslanbay@users.noreply.github.com>
2026-07-24 10:28:40 +0000

    feat(types): create global type declarations for d3 and Plotly

---
+++ typescript/global.d.ts
@@ -0,0 +1,39 @@
+import type * as D3 from 'd3';
+
+declare global {
+  const d3: typeof D3;
+
+  namespace Plotly {
+    interface PlotlyHTMLElement extends HTMLElement {
+      on(event: string, callback: (data: any) => void): void;
+      removeAllListeners(event: string): void;
+    }
+
+    type Layout = Record<string, any>;
+    type Data = Record<string, any>[];
+    type Config = Record<string, any>;
+
+    function newPlot(
+      root: string | HTMLElement,
+      data: Data,
+      layout?: Partial<Layout>,
+      config?: Partial<Config>,
+    ): Promise<PlotlyHTMLElement>;
+
+    function react(
+      root: string | HTMLElement,
+      data: Data,
+      layout?: Partial<Layout>,
+      config?: Partial<Config>,
+    ): Promise<PlotlyHTMLElement>;
+
+    function purge(root: string | HTMLElement): void;
+
+    function relayout(
+      root: string | HTMLElement,
+      update: Partial<Layout>,
+    ): Promise<PlotlyHTMLElement>;
+  }
+}
+
+export {};
